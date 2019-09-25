d9687453e8f62ed472ed3abaabe343b6b077eed3
suzuki-ai <karikakko10@gmail.com>
2019-09-25 15:02:49 +0900

    課題B-2: 長方形オブジェクト クラスを実装して長方形の面積及び対角線の長さを返すプログラムの作成 クラス内に長方形の面積を求める関数　対角線の長さを返す関数を定義
    それぞれreturnにそれぞれの値を出力するprint文を記入

--- b-2.py
+++ b-2.py
@@ -10,10 +10,32 @@ rectangle2 = Rectangle(height=3, width=3)
 rectangle2.area()  # 9.00
 rectangle2.diagonal()  # 4.24
 """
+import math
+
+
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return print(f'{self.height * self.width:.2f}')
+
+    def diagonal(self):
+        return print(f'{math.sqrt(self.height ** 2 + self.width ** 2):.2f}')
 
 
 def main():
-    pass
+    # 入力
+
+    # 出力
+    rectangle1 = Rectangle(height=5, width=6)
+    rectangle1.area()  # 30.00
+    rectangle1.diagonal()  # 7.81
+
+    rectangle2 = Rectangle(height=3, width=3)
+    rectangle2.area()  # 9.00
+    rectangle2.diagonal()  # 4.24
 
 
 if __name__ == '__main__':
