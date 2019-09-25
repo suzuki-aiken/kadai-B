da85178377bacecd5647f93e8621f6ca88cebb5f
suzuki-ai <karikakko10@gmail.com>
2019-09-25 14:42:27 +0900

    課題B-2: 長方形オブジェクト 開始

---
+++ b-2.py
@@ -0,0 +1,20 @@
+"""
+課題B-2: 長方形オブジェクト
+次のコードが正しく動作するような Rectangle クラスを実装してください
+diagonal は 対角線(の長さ) という意味です。
+rectangle1 = Rectangle(height=5, width=6)
+rectangle1.area()  # 30.00
+rectangle1.diagonal()  # 7.81
+
+rectangle2 = Rectangle(height=3, width=3)
+rectangle2.area()  # 9.00
+rectangle2.diagonal()  # 4.24
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
