a8f0c12e45e38997fcbe9af688621896d4b1f099
suzuki-ai <karikakko10@gmail.com>
2019-09-25 15:33:36 +0900

    課題B-5 引用される回数　与えられた数値と数字を引き算するクラスMyCounterV3の実装 完成

---
+++ b-5.py
@@ -0,0 +1,32 @@
+"""
+次のコードが正しく動作するような MyCounterV3 クラスを実装してください
+counter1 = MyCounterV3(value=1, step=2)
+print(counter1.value)  # 1
+
+counter1.count_up()
+print(counter1.value)  # 3
+
+counter1.count_up()
+print(counter1.value)  # 5
+
+counter1.count_down()
+print(counter1.value)  # 3
+
+counter2 = MyCounterV3(value=3, step=4)
+print(counter2.value)  # 3
+
+counter2.count_down()
+print(counter2.value)  # -1
+
+counter2.count_down()
+print(counter2.value)  # -5
+"""
+class MyCounterV3:
+    def __init__(self):
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
