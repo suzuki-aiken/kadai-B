e6ee614046656871e8ec0c239bdbf892da6387c0
suzuki-ai <karikakko10@gmail.com>
2019-09-25 15:07:25 +0900

    与えられた数値から引用される回数数字をカウントするクラスMyCounterV1の実装 開始

---
+++ b-3.py
@@ -0,0 +1,45 @@
+"""
+課題B-3: カウンターその1
+次のコードが正しく動作するような MyCounterV1 クラスを実装してください
+counter1 = MyCounterV1(value=0)
+print(counter1.value)  # 0
+
+counter1.count_up()
+print(counter1.value)  # 1
+
+counter1.count_up()
+print(counter1.value)  # 2
+
+counter2 = MyCounterV1(value=7)
+print(counter2.value)  # 7
+
+counter2.count_up()
+print(counter2.value)  # 8
+
+counter2.count_up()
+print(counter2.value)  # 9
+"""
+
+class MyCounterV1
+
+def main():
+    counter1 = MyCounterV1(value=0)
+    print(counter1.value)  # 0
+
+    counter1.count_up()
+    print(counter1.value)  # 1
+
+    counter1.count_up()
+    print(counter1.value)  # 2
+
+    counter2 = MyCounterV1(value=7)
+    print(counter2.value)  # 7
+
+    counter2.count_up()
+    print(counter2.value)  # 8
+
+    counter2.count_up()
+    print(counter2.value)  # 9
+
+if __name__ == '__main__':
+    main()
